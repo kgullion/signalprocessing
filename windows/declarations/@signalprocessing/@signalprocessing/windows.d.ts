a4ec8303369ae9d3302cc90cd6a1da42da05b611
Your Name <you@example.com>
2021-01-26 00:57:49 +0300

    minors

--- windows/declarations/@signalprocessing/@signalprocessing/windows.d.ts
+++ windows/declarations/@signalprocessing/@signalprocessing/windows.d.ts
@@ -5,13 +5,54 @@
  */
 
 declare module "@signalprocessing/windows" {
+  /**
+   *
+   * @param L
+   * @param type
+   */
   export function hamming(L: number, type?: string): number[];
+  /**
+   *
+   * @param L
+   * @param type
+   */
   export function hann(L: number, type?: string): number[];
+  /**
+   *
+   * @param L
+   * @param type
+   */
   export function flattop(L: number, type?: string): number[];
+  /**
+   *
+   * @param L
+   */
   export function rect(L: number): number[];
+  /**
+   *
+   * @param L
+   */
   export function barthann(L: number): number[];
+  /**
+   *
+   * @param L
+   */
   export function bartlett(L: number): number[];
+  /**
+   *
+   * @param L
+   * @param type
+   */
   export function blackman(L: number, type?: string): number[];
+  /**
+   *
+   * @param L
+   * @param type
+   */
   export function blackmanharris(L: number, type?: string): number[];
+  /**
+   *
+   * @param L
+   */
   export function bohman(L: number): number[];
 }
